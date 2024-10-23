f4c63dffab0e0524ffe8f0a5963d43005d473c9d
Julia Polyakova <razvlechenia.svoboda@yandex.ru>
2024-10-23 13:20:37 +0500

    create project

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:20.12.2 as build
+WORKDIR /app/src
+COPY package*.json ./
+
+RUN npm install -g @angular/cli
+RUN npm install --legacy-peer-deps
+
+COPY . ./
+
+RUN npm run build
+
+FROM node:20.12.2
+WORKDIR /usr/app
+COPY --from=build /app/src/dist/trainer-journal-frontend/ ./
+CMD node server/server.mjs
+EXPOSE 4200
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+    title = 'trainer-journal-frontend';
+}

61a6398b1e162c185902238cfca2a1de8f302648
Julia Polyakova <razvlechenia.svoboda@yandex.ru>
2024-11-03 20:07:42 +0500

    feat: add auth request/response models

---
+++ src/app/data/request-models/auth/IAuth.request-model.ts
@@ -0,0 +1,4 @@
+export interface IAuthRequestModel {
+    readonly userName: string,
+    readonly password: string,
+}
---
+++ src/app/data/response-models/auth/IAuth.response-model.ts
@@ -0,0 +1,5 @@
+export interface IAuthResponseModel {
+    readonly id: string,
+    readonly userName: string,
+    readonly token: string,
+}

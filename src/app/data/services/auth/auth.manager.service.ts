fce75ea793e8f6061453a53bf2a359a14e312c5c
Julia Polyakova <razvlechenia.svoboda@yandex.ru>
2024-11-03 22:15:28 +0500

    chore: update auth manager service to add error handler

--- src/app/data/services/auth/auth.manager.service.ts
+++ src/app/data/services/auth/auth.manager.service.ts
@@ -1,14 +1,13 @@
-import {ErrorHandler, inject, Injectable} from '@angular/core';
+import {inject, Injectable} from '@angular/core';
 import {AuthService} from './auth.service';
 import {IAuthRequestModel} from '../../request-models/auth/IAuth.request-model';
-import {catchError, map, NEVER, Observable} from 'rxjs';
+import {catchError, map, Observable, throwError} from 'rxjs';
 import {IAuthResponseModel} from '../../response-models/auth/IAuth.response-model';
 
 @Injectable()
 export class AuthManagerService {
 
     private readonly _authService: AuthService = inject(AuthService);
-    private readonly _errorHandler: ErrorHandler = inject(ErrorHandler);
     private _token: string | null = null;
 
     public loginUser(user: IAuthRequestModel): Observable<boolean> {
@@ -19,8 +18,7 @@ export class AuthManagerService {
                 return true;
             }),
             catchError(err => {
-                this._errorHandler.handleError(err);
-                return NEVER;
+                return this.handleLoginError(err);
             })
         );
     }
@@ -28,5 +26,17 @@ export class AuthManagerService {
     public logout(): void {
         this._token = null;
         localStorage.removeItem('token');
+    }
+
+    private handleLoginError(err: any): Observable<never> {
+        const errorMessage: string = this.getErrorMessage(err);
+        return throwError(() => new Error(errorMessage));
+    }
+
+    private getErrorMessage(err: any): string {
+        if (err.status == 404) {
+            return "Ошибка. Неправильный логин и/или пароль.";
+        }
+        return "Произошла ошибка. Попробуйте еще раз.";
     }
 }

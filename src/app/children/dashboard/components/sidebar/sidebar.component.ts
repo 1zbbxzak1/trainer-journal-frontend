b35e7800da61550ae011986cf0fced390fc91203
Julia Polyakova <razvlechenia.svoboda@yandex.ru>
2024-11-04 21:58:28 +0500

    feat: add sidebar

---
+++ src/app/children/dashboard/components/sidebar/sidebar.component.ts
@@ -0,0 +1,12 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {StateBarService} from '../../../services/state-bar/state-bar.service';
+
+@Component({
+    selector: 'app-sidebar',
+    templateUrl: './sidebar.component.html',
+    styleUrl: './styles/sidebar.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SidebarComponent extends StateBarService {
+
+}
--- src/app/children/dashboard/dashboard.module.ts
+++ src/app/children/dashboard/dashboard.module.ts
@@ -1,14 +1,20 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {SidebarComponent} from './components/sidebar/sidebar.component';
 import {TeamsComponent} from './pages/teams/teams.component';
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 
 @NgModule({
     declarations: [
+        SidebarComponent,
         TeamsComponent,
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        RouterLink,
+        RouterLinkActive,
+        NgOptimizedImage
     ]
 })
 export class DashboardModule {

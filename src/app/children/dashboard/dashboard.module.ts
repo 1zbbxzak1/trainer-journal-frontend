1b3a8dd946c1e98f87a63b7a2fa8ef49e457c81c
Julia Polyakova <razvlechenia.svoboda@yandex.ru>
2024-11-04 22:00:37 +0500

    feat: add page stubs

--- src/app/children/dashboard/dashboard.module.ts
+++ src/app/children/dashboard/dashboard.module.ts
@@ -2,13 +2,19 @@ import {NgModule} from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
 import {SidebarComponent} from './components/sidebar/sidebar.component';
 import {TeamsComponent} from './pages/teams/teams.component';
+import {ScheduleComponent} from './pages/schedule/schedule.component';
 import {RouterLink, RouterLinkActive} from "@angular/router";
+import {ChecksComponent} from './pages/checks/checks.component';
+import {ProfileComponent} from './pages/profile/profile.component';
 
 
 @NgModule({
     declarations: [
         SidebarComponent,
         TeamsComponent,
+        ScheduleComponent,
+        ChecksComponent,
+        ProfileComponent,
     ],
     imports: [
         CommonModule,
--- src/app/router.config.ts
+++ src/app/router.config.ts
@@ -3,6 +3,9 @@ import {AuthGuard} from './data/guards/auth.guard';
 import {inject} from '@angular/core';
 import {AuthComponent} from './children/authorization/pages/auth/auth.component';
 import {TeamsComponent} from './children/dashboard/pages/teams/teams.component';
+import {ScheduleComponent} from './children/dashboard/pages/schedule/schedule.component';
+import {ChecksComponent} from './children/dashboard/pages/checks/checks.component';
+import {ProfileComponent} from './children/dashboard/pages/profile/profile.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'dashboard/teams', pathMatch: 'full'},
@@ -12,8 +15,23 @@ export const routes: Routes = [
         canActivate: [(router: ActivatedRouteSnapshot, state: RouterStateSnapshot) => inject(AuthGuard).canActivate(router, state)],
     },
     {
+        path: 'dashboard/schedule',
+        component: ScheduleComponent,
+        canActivate: [(router: ActivatedRouteSnapshot, state: RouterStateSnapshot) => inject(AuthGuard).canActivate(router, state)],
+    },
+    {
         path: 'dashboard/teams',
         component: TeamsComponent,
+        canActivate: [(router: ActivatedRouteSnapshot, state: RouterStateSnapshot) => inject(AuthGuard).canActivate(router, state)],
+    },
+    {
+        path: 'dashboard/checks',
+        component: ChecksComponent,
+        canActivate: [(router: ActivatedRouteSnapshot, state: RouterStateSnapshot) => inject(AuthGuard).canActivate(router, state)],
+    },
+    {
+        path: 'dashboard/profile',
+        component: ProfileComponent,
         canActivate: [(router: ActivatedRouteSnapshot, state: RouterStateSnapshot) => inject(AuthGuard).canActivate(router, state)],
     },
 ];

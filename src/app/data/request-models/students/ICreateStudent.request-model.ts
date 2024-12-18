4172fca22b43d69cb59d4baa6c2536178061e5a5
Julia Polyakova <razvlechenia.svoboda@yandex.ru>
2024-12-18 07:30:36 +0500

    feat: add response models for schedule

--- src/app/data/request-models/students/ICreateStudent.request-model.ts
+++ src/app/data/request-models/students/ICreateStudent.request-model.ts
@@ -3,10 +3,10 @@ import {IContactsModel} from '../../models/students/IContacts.model';
 export interface ICreateStudentRequestModel {
     readonly fullName: string,
     readonly gender: string,
-    readonly birthDate: Date,
-    readonly schoolGrade: number,
-    readonly kyu: number | null,
-    readonly address: string | null,
+    readonly birthDate: string,
+    readonly schoolGrade?: number,
+    readonly kyu?: number | null,
+    readonly address: string,
     readonly groupIds: string[],
     readonly contacts: IContactsModel[],
 }
--- src/app/data/response-models/students/IStudentItem.response-model.ts
+++ src/app/data/response-models/students/IStudentItem.response-model.ts
@@ -4,10 +4,12 @@ export interface IStudentItemResponseModel {
     readonly username: string,
     readonly fullName: string,
     readonly balance: number,
+    readonly birthDate: Date,
     readonly age: number,
     readonly schoolGrade: number,
     readonly kyu: number | null,
     readonly gender: string,
+    readonly address: string,
     readonly groupIds: string[],
     readonly contact: IContactsModel,
 }
